96bb0dd34df4bc9030c9bfa87806123d1e43280f
jonathankerth <jonathanpkerth@gmail.com>
2024-01-04 22:02:58 -0800

    fix eslint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,30 +4,14 @@ module.exports = {
 		node: true,
 		browser: true,
 	},
-	extends: [
-		"plugin:vue/vue3-essential", // or 'plugin:vue/vue3-strongly-recommended' or 'plugin:vue/vue3-recommended' for stricter rules
-		"eslint:recommended",
-	],
+	extends: ["plugin:vue/vue3-essential", "eslint:recommended"],
 	parserOptions: {
-		parser: "babel-eslint",
+		parser: "@babel/eslint-parser",
+		requireConfigFile: false,
 	},
 	rules: {
 		"vue/no-unused-vars": "warn",
 		"no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
 		"no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-
-		"at-rule-no-unknown": [
-			true,
-			{
-				ignoreAtRules: [
-					"tailwind",
-					"apply",
-					"variants",
-					"responsive",
-					"screen",
-				],
-			},
-		],
 	},
 };
-``;
