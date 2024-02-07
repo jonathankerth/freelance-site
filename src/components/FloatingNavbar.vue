dfebb2702559636f2b4a65e921db396086d08cca
jonathankerth <jonathanpkerth@gmail.com>
2024-02-07 14:09:31 -0800

    add toggle function

--- src/components/FloatingNavbar.vue
+++ src/components/FloatingNavbar.vue
@@ -1,16 +1,17 @@
 <template>
-	<div
-		v-if="!isNavbarVisible"
-		class="floating-navbar"
-		@click="toggleVisibility"
-	>
-		<ul>
-			<li @click.stop="scrollTo('home-offer-section')">Services</li>
-			<li @click.stop="scrollTo('about-us')">About</li>
-			<li @click.stop="scrollTo('past-work')">Work</li>
-			<li @click.stop="scrollTo('book-appointment')">Book</li>
-			<li @click.stop="scrollTo('my-contacts')">Contact</li>
-		</ul>
+	<div v-if="!isNavbarVisible" class="floating-navbar">
+		<div class="navbar-content">
+			<div @click="toggleVisibility" class="toggle-button">
+				<span>&#x2022;&#x2022;&#x2022;</span>
+			</div>
+			<ul v-if="isExpanded">
+				<li @click.stop="scrollTo('home-offer-section')">Services</li>
+				<li @click.stop="scrollTo('about-us')">About</li>
+				<li @click.stop="scrollTo('past-work')">Work</li>
+				<li @click.stop="scrollTo('book-appointment')">Book</li>
+				<li @click.stop="scrollTo('my-contacts')">Contact</li>
+			</ul>
+		</div>
 	</div>
 </template>
 
@@ -23,7 +24,7 @@ export default {
 	data() {
 		return {
 			isNavbarVisible: true,
-			isExpanded: true, // Add state to manage visibility on mobile
+			isExpanded: true,
 		};
 	},
 	methods: {
@@ -49,18 +50,40 @@ export default {
 .floating-navbar {
 	position: fixed;
 	right: 20px;
-	top: 50%;
+	top: 20%;
 	transform: translateY(-50%);
-	background-color: rgba(26, 32, 44, 0.8);
-	padding: 10px;
+	background-color: rgba(139, 139, 139, 0.8);
+	padding: 4px;
 	border-radius: 5px;
 	z-index: 1000;
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+}
+
+.navbar-content {
+	display: flex;
+	align-items: center;
+}
+
+.toggle-button {
+	cursor: pointer;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	width: 20px;
+	height: 20px;
+	font-size: 20px;
+	line-height: 0;
+	writing-mode: vertical-lr;
+	transform: rotate(180deg);
+	margin: 0;
 }
 
 .floating-navbar ul {
 	list-style: none;
 	margin: 0;
-	padding: 0;
+	display: true;
 }
 
 .floating-navbar li {
@@ -68,16 +91,15 @@ export default {
 	transition: color 0.3s ease;
 	color: rgba(255, 255, 255, 0.7);
 	margin-bottom: 5px;
-	text-align: center;
+	text-align: right;
 }
 
 .floating-navbar li:hover {
 	color: #ffffff;
 }
 
-@media (max-width: 768px) {
-	.floating-navbar {
-		display: none;
-	}
+.floating-navbar:hover ul,
+.floating-navbar:focus-within ul {
+	display: block;
 }
 </style>
